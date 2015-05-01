5f292ca9700cd63c1da8caa90288451983934756
Guilherme Stiebler <guilhermemst@gmail.com>
2015-05-01 13:50:19 -0300

    using external json converter

--- autocad_plugin/ExportCommand.cs
+++ autocad_plugin/ExportCommand.cs
@@ -7,8 +7,12 @@ using Autodesk.AutoCAD.DatabaseServices;
 using Autodesk.AutoCAD.Geometry;
 using Autodesk.AutoCAD.EditorInput;
 
+using Newtonsoft.Json;
+using System.Collections;
+
 // This line is not mandatory, but improves loading performances
 [assembly: CommandClass(typeof(AutoCAD_CSharp_plug_in1.MyCommands))]
+
 
 namespace AutoCAD_CSharp_plug_in1
 {
@@ -20,171 +24,23 @@ namespace AutoCAD_CSharp_plug_in1
     public class MyCommands
     {
 
-        public static void writeJsonString( System.IO.StreamWriter file, string key, string value )
+        public static cwConversor.BaseObj getCWObject(DBObject dbo, int id)
         {
-            file.WriteLine("\"" + key + "\": " + "\"" + value + "\",");
-        }
-
-
-        public static void writeJsonNumber(System.IO.StreamWriter file, string key, double number, bool hasFinalComma)
-        {
-            string text = "\"" + key + "\": " + number;
-            if(hasFinalComma)
-                text += ",";
-            file.WriteLine(text);
-        }
-
-
-        public static void writeJsonInteger(System.IO.StreamWriter file, string key, int number, bool hasFinalComma)
-        {
-            string text = "\"" + key + "\": " + number;
-            if (hasFinalComma)
-                text += ",";
-            file.WriteLine(text);
-        }
-
-
-        public static void writeCircle(System.IO.StreamWriter file, Circle circle)
-        {
-            writeJsonString(file, "OBJ", "CIRCLE");
-            writeJsonString(file, "LAYER", circle.Layer);
-            writeJsonNumber(file, "DIAMETER", circle.Diameter, true);
-            writeJsonNumber(file, "CENTER_X", circle.Center.X, true);
-            writeJsonNumber(file, "CENTER_Y", circle.Center.Y, true);
-        }
-
-
-
-        public static void writeArc(System.IO.StreamWriter file, Arc arc)
-        {
-            writeJsonString(file, "OBJ", "ARC");
-            writeJsonString(file, "LAYER", arc.Layer);
-            writeJsonNumber(file, "CENTER_X", arc.Center.X, true);
-            writeJsonNumber(file, "CENTER_Y", arc.Center.Y, true);
-            writeJsonNumber(file, "END_POINT_X", arc.EndPoint.X, true);
-            writeJsonNumber(file, "END_POINT_Y", arc.EndPoint.Y, true);
-            writeJsonNumber(file, "START_ANGLE", arc.StartAngle, true);
-            writeJsonNumber(file, "END_ANGLE", arc.EndAngle, true);
-        }
-
-
-        
-        public static void writeLine(System.IO.StreamWriter file, Line line)
-        {
-            writeJsonString(file, "OBJ", "LINE");
-            writeJsonString(file, "LAYER", line.Layer);
-            writeJsonNumber(file, "START_POINT_X", line.StartPoint.X, true);
-            writeJsonNumber(file, "START_POINT_Y", line.StartPoint.Y, true);
-            writeJsonNumber(file, "END_POINT_X", line.EndPoint.X, true);
-            writeJsonNumber(file, "END_POINT_Y", line.EndPoint.Y, true);
-        }
-
-
-
-        public static void writePolyline(System.IO.StreamWriter file, Polyline pline)
-        {
-            writeJsonString(file, "OBJ", "PLINE");
-            writeJsonString(file, "LAYER", pline.Layer);
-            writeJsonInteger(file, "NUM_VERTEX", pline.NumberOfVertices, true);
-            file.WriteLine("\"vertices\": [");
-            for (int i = 0; i < pline.NumberOfVertices; i++)
-            {
-                double x = pline.GetPoint2dAt(i).X;
-                double y = pline.GetPoint2dAt(i).Y;
-                file.WriteLine("{");
-                writeJsonNumber(file, "X", x, true);
-                writeJsonNumber(file, "Y", y, false);
-                if (i == pline.NumberOfVertices - 1)
-                    file.WriteLine("}");
-                else
-                    file.WriteLine("},");
-            }
-            file.WriteLine("],");
-        }
-
-
-
-        public static string replaceBreakString( string input )
-        {
-            return input.Replace( "\r\n", " " );
-        }
-
-
-        public static string replaceAspasString(string input)
-        {
-            return input.Replace("\"", "<*aspas*>");
-        }
-
-
-        public static string normalizeOutputText(string input)
-        {
-            return replaceAspasString( replaceBreakString(input) );
-        }
-
-
-        public static void writeText(System.IO.StreamWriter file, MText text)
-        {
-            writeJsonString(file, "OBJ", "TEXT");
-            writeJsonString(file, "LAYER", text.Layer);
-            writeJsonString(file, "TEXT", normalizeOutputText(text.Text));
-            writeJsonNumber(file, "X", text.Location.X, true);
-            writeJsonNumber(file, "Y", text.Location.Y, true);
-            writeJsonNumber(file, "WIDTH_FACTOR", text.Width, true);
-        }
-
-
-
-        public static void writeDBText(System.IO.StreamWriter file, DBText text)
-        {
-            writeJsonString(file, "OBJ", "DBTEXT");
-            writeJsonString(file, "LAYER", text.Layer);
-            writeJsonString(file, "TEXT", normalizeOutputText(text.TextString));
-            writeJsonNumber(file, "X", text.Position.X, true);
-            writeJsonNumber(file, "Y", text.Position.Y, true);
-            writeJsonNumber(file, "WIDTH_FACTOR", text.WidthFactor, true);
-        }
-
-
-        public static void writeObject(System.IO.StreamWriter file, DBObject dbo, int id, bool hasFinalComma)
-        {
-            file.WriteLine("{");
+            cwConversor.BaseObj obj = null;
             if (dbo is Circle)
-            {
-                Circle circle = dbo as Circle;
-                writeCircle(file, circle);
-            }
+                obj = new cwConversor.Circle(dbo as Circle, id);
             else if (dbo is Arc)
-            {
-                Arc arc = dbo as Arc;
-                writeArc(file, arc);
-            }
+                obj = new cwConversor.Arc(dbo as Arc, id);
             else if (dbo is Line)
-            {
-                Line line = dbo as Line;
-                writeLine(file, line);
-            }
+                obj = new cwConversor.Line(dbo as Line, id);
             else if (dbo is MText)
-            {
-                MText text = dbo as MText;
-                writeText( file, text );
-            }
+                obj = new cwConversor.Text(dbo as MText, id);
             else if (dbo is DBText)
-            {
-                DBText text = dbo as DBText;
-                writeDBText(file, text);
-            }
+                obj = new cwConversor.Text(dbo as DBText, id);
             else if (dbo is Polyline)
-            {
-                Polyline pline = dbo as Polyline;
-                writePolyline( file, pline );
-            }
-            else
-                file.WriteLine("\"__type not processed\": \"" + dbo.GetType() + "\"," );
-            writeJsonInteger(file, "ID", id, false);
-            if (hasFinalComma)
-                file.WriteLine("},");
-            else
-                file.WriteLine("}");
+                obj = new cwConversor.Polyline(dbo as Polyline, id);
+
+            return obj;
         }
 
 
@@ -200,6 +56,8 @@ namespace AutoCAD_CSharp_plug_in1
         [CommandMethod("dwg2json")]
         public static void dwg2json()
         {
+            cwConversor.JsonDwg jsonDwg = new cwConversor.JsonDwg();
+
             // Get the current document and database, and start a transaction
             Document acDoc = Application.DocumentManager.MdiActiveDocument;
             string outputFileName;
@@ -230,34 +88,25 @@ namespace AutoCAD_CSharp_plug_in1
                     int id = 1;
                     System.Collections.Generic.HashSet<DBObject> usedObjects = new System.Collections.Generic.HashSet<DBObject>();
 
-                    file.WriteLine("{ \"groups\": [");
-                    int counter1 = 0;
                     foreach (DBDictionaryEntry dictEntry in groups)
                     {
+                        cwConversor.Group cwGroup = new cwConversor.Group();
                         DBObject dbo = acTrans.GetObject(dictEntry.Value, OpenMode.ForRead);
                         Group group = dbo as Group;
                         ObjectId[] ids = group.GetAllEntityIds();
                         int l = ids.Length;
-                        file.WriteLine("[");
                         for (int i = 0; i < l; ++i )
                         {
                             ObjectId id2 = ids[i];
                             DBObject dboInG = acTrans.GetObject(id2, OpenMode.ForRead);
-                            writeObject(file, dboInG, id++, i < (l - 1));
+                            cwConversor.BaseObj obj = getCWObject(dboInG, id++);
                             usedObjects.Add(dboInG);
+                            cwGroup.objects.Add( obj );
                         }
-                        counter1++;
-                        if (counter1 == groups.Count)
-                            file.WriteLine("]");
-                        else
-                            file.WriteLine("],");
+                        jsonDwg.groups.Add( group );
                     }
-                    file.WriteLine("],");
 
-                    int nCnt = 0;
-
-                    file.WriteLine("\"objects\": [");
-                    // Step through each object in Model space and
+                                        // Step through each object in Model space and
                     // display the type of object found
                     foreach (ObjectId acObjId in acBlkTblRec)
                     {
@@ -266,24 +115,16 @@ namespace AutoCAD_CSharp_plug_in1
                         if (usedObjects.Contains(dbo))
                             continue;
                         
-                        nCnt = nCnt + 1;
-                        writeObject(file, dbo, id++, true);
-                    }
-                    file.WriteLine("{\"dummy\": \"dummy\"}]");
-                    file.WriteLine("}");
-
-                    // If no objects are found then display a message
-                    if (nCnt == 0)
-                    {
-                        file.WriteLine("\n No objects found");
+                        cwConversor.BaseObj obj = getCWObject(dbo, id++);
+                        jsonDwg.objects.Add( obj );
                     }
 
                     // Dispose of the transaction
                 }
+
+                string output = JsonConvert.SerializeObject(jsonDwg);
+                file.WriteLine(output);
             }
-
-
-           
         }
 
     }
---
+++ autocad_plugin/cwConversor.cs
@@ -0,0 +1,139 @@
+﻿using Autodesk.AutoCAD.Geometry;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cwConversor
+{
+    public class Group
+    {
+        public ArrayList objects;
+
+        public Group()
+        {
+            objects = new ArrayList();
+        }
+    }
+
+    public class BaseObj
+    {
+        public string layer;
+        public int id;
+
+        public BaseObj(Autodesk.AutoCAD.DatabaseServices.Entity entity, int id_)
+        {
+            layer = entity.Layer;
+            id = id_;
+        }
+    }
+
+    public class Point
+    {
+        public double x;
+        public double y;
+
+        public Point(Point3d p)
+        {
+            x = p.X;
+            y = p.Y;
+        }
+
+        public Point(Point2d p)
+        {
+            x = p.X;
+            y = p.Y;
+        }
+    }
+
+    public class Line : BaseObj
+    {
+        public Point start;
+        public Point end;
+
+        public Line(Autodesk.AutoCAD.DatabaseServices.Line line, int id)
+            : base(line, id)
+        {
+            start = new Point(line.StartPoint);
+            end = new Point(line.EndPoint);
+        }
+    }
+
+    public class Circle : BaseObj
+    {
+        public double diameter;
+        public Point center;
+
+        public Circle(Autodesk.AutoCAD.DatabaseServices.Circle circle, int id)
+            : base(circle, id)
+        {
+            diameter = circle.Diameter;
+            center = new Point(circle.Center);
+        }
+    }
+
+    public class Arc : BaseObj
+    {
+        public Point center;
+        public Point endPoint;
+        public double startAngle;
+        public double endAngle;
+
+        public Arc(Autodesk.AutoCAD.DatabaseServices.Arc arc, int id)
+            : base(arc, id)
+        {
+            center = new Point(arc.Center);
+            endPoint = new Point(arc.EndPoint);
+            startAngle = arc.StartAngle;
+            endAngle = arc.EndAngle;
+        }
+    }
+
+    public class Polyline : BaseObj
+    {
+        public Point[] points;
+
+        public Polyline(Autodesk.AutoCAD.DatabaseServices.Polyline polyline, int id)
+            : base(polyline, id)
+        {
+            points = new Point[polyline.NumberOfVertices];
+            for (int i = 0; i < polyline.NumberOfVertices; i++)
+                points[i] = new Point(polyline.GetPoint2dAt(i));
+        }
+    }
+
+    public class Text : BaseObj
+    {
+        public Point location;
+        public string text;
+        public double factor;
+
+        public Text(Autodesk.AutoCAD.DatabaseServices.MText textObj, int id)
+            : base(textObj, id)
+        {
+            location = new Point(textObj.Location);
+            text = textObj.Text;
+            factor = textObj.Width;
+        }
+
+        public Text(Autodesk.AutoCAD.DatabaseServices.DBText textObj, int id)
+            : base(textObj, id)
+        {
+            location = new Point(textObj.Position);
+            text = textObj.TextString;
+            factor = textObj.WidthFactor;
+        }
+    }
+
+    public class JsonDwg : Group
+    {
+        public ArrayList groups;
+
+        public JsonDwg()
+        {
+            groups = new ArrayList();
+        }
+    }
+}
