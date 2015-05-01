1e5a3bff7a12d081e158a0698bc8e2c11052976c
Guilherme Stiebler <guilhermemst@gmail.com>
2015-05-01 11:12:29 -0300

    cable type on bandeirola report

--- src/Reports.cpp
+++ src/Reports.cpp
@@ -43,7 +43,7 @@ void Reports::generateCirtuitsReport( std::string fileName, const std::vector<In
 
 void Reports::generateBandeirolaReport( std::string fileName, const std::vector<InputCircuit> &inputCircuits, const std::vector<CircuitResult> &resultCircuits )
 {
-	map<string, vector<string> > circuitsOfBandeirola;
+	map<string, vector<InputCircuit> > circuitsOfBandeirola;
 	for( int i(0); i < inputCircuits.size(); ++i )
 	{
 		const InputCircuit &inputCircuit = inputCircuits[i];
@@ -53,24 +53,24 @@ void Reports::generateBandeirolaReport( std::string fileName, const std::vector<
 			continue;
 
 		for(int j(1); j < resultCircuit.route.size() - 1; ++j)
-			circuitsOfBandeirola[ resultCircuit.route[j] ].push_back( inputCircuit.name );
+			circuitsOfBandeirola[ resultCircuit.route[j] ].push_back( inputCircuit );
 	}
 
 	QFile file( QString::fromLatin1( fileName.c_str() ) );
     file.open(QIODevice::WriteOnly | QIODevice::Text);
     QTextStream out(&file);
  
-	map<string, vector<string> >::iterator it, e = circuitsOfBandeirola.end();
+	map<string, vector<InputCircuit> >::iterator it, e = circuitsOfBandeirola.end();
 	for( it = circuitsOfBandeirola.begin(); it != e; it++)
 	{
 		string line = "Bandeirola: " + it->first + ";\n";
 		out << QString::fromLatin1( line.c_str() );
 
-		vector<string> &circuits = it->second;
+		vector<InputCircuit> &circuits = it->second;
 		for(int i(0); i < circuits.size(); ++i)
 		{
-			out << QString::fromLatin1( circuits[i].c_str() );
-			out << QString::fromLatin1( ";\n" );
+			out << QString::fromUtf8( circuits[i].name.c_str() ) + ";";
+			out << QString::fromUtf8( circuits[i].cable.c_str() ) + ";\n";
 		}
 	
 		out << "\n";
