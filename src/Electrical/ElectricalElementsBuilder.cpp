cf674f5176f1b79f14572c819cf3be46af59a990
Guilherme Stiebler <guilhermemst@gmail.com>
2015-09-25 18:16:21 -0300

    first steps of electrical elements builder

--- src/Electrical/ElectricalElementsBuilder.cpp
+++ src/Electrical/ElectricalElementsBuilder.cpp
@@ -1,1 +1,41 @@
 #include "ElectricalElementsBuilder.h"
+
+#include "UDadosGenerico.h"
+#include "StraightCable.h"
+#include "ArcCable.h"
+
+using namespace std;
+
+void ElectricalElementsBuilder::build( std::shared_ptr<CDadosGenerico> drawingData, std::shared_ptr<ElectricalElements> electricalElements )
+{
+	buildStraightCable( drawingData->Multipoint, electricalElements->_straightCables );
+	buildArcCable( drawingData->Arcos, electricalElements->_arcCables );
+}
+
+
+void ElectricalElementsBuilder::buildStraightCable( vector< shared_ptr<TMultipoint> > &multiPoints, vector< shared_ptr<StraightCable> > &straightCables )
+{
+	for (int n = 0; n < multiPoints.size(); n++)
+    {
+        if ( multiPoints[n]->Nivel == CABO )
+        {
+			shared_ptr<StraightCable> straightCable = make_shared<StraightCable>();
+			straightCables.push_back( straightCable );
+			straightCable->_multipoint = multiPoints[n];
+        }
+    }
+}
+
+
+void ElectricalElementsBuilder::buildArcCable( std::vector< std::shared_ptr<TArco> > &arcs, std::vector< shared_ptr<ArcCable> > &arcCables )
+{
+	for (int n = 0; n < arcs.size(); n++)
+    {
+		if ( arcs[n]->Nivel == CABO )
+        {
+			shared_ptr<ArcCable> arcCable = make_shared<ArcCable>();
+			arcCable->_arco = arcs[n];
+			arcCables.push_back( arcCable );
+        }
+    }
+}
--- src/Electrical/ElectricalElementsBuilder.h
+++ src/Electrical/ElectricalElementsBuilder.h
@@ -1,10 +1,33 @@
 #ifndef ELECTRICAL_ELEMENTS_BUILDER_H__
 #define ELECTRICAL_ELEMENTS_BUILDER_H__
 
+#include <memory>
+#include "ElectricalElements.h"
+
+class CDadosGenerico;
+class StraightCable;
+class ArcCable;
+struct TMultipoint;
+struct TArco;
+
 class ElectricalElementsBuilder
 {
 public:
+	/**
+	 * Generates the electrical elements from the drawing data
+	 */
+	static void build( std::shared_ptr<CDadosGenerico> drawingData, std::shared_ptr<ElectricalElements> electricalElements );
 
+private:
+	/**
+	 * Builds the straight cables array
+	 */
+	static void buildStraightCable( std::vector< std::shared_ptr<TMultipoint> > &multiPoints, std::vector< std::shared_ptr<StraightCable> > &straightCables  );
+
+	/**
+	 * Builds the arc cables array
+	 */
+	static void buildArcCable( std::vector< std::shared_ptr<TArco> > &arcs, std::vector< std::shared_ptr<ArcCable> > &arcCables );
 };
 
 #endif
